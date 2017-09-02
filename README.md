125cbe072e9b2f78cc9999a8368c5d343f1ef136
unknown <Valarie.Regas@gmail.com>
2017-09-01 21:18:18 -0400

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+This project is a guessing game in which the player is given a target number to begin. Each of four unlabeled images has a set value, unknown to the player at the beginning. Only by clicking the images and being told their running total score can the player determine the value of each image, so that they might click the correct images for their score to reach the target integer given. 
+
+From the game:
+
+“Crystal Clarity
+
+Come and seek enlightenment with a few of your friendly neighborhood deities!
+
+Each unearthly ruler has been assigned a numerical value, much in the same manner as each person on earth has an intrinsic value. Click a god to see their worth reflected in the current number. There will be math involved here, my children; but only because we know you are sufficiently strong to endure this plight.
+
+Aim to achieve an ever-higher score, up to and including the target number given you.
+
+But beware! You must reach the target number to win, but you may not go higher, or (not unlike a prophet who feels himself more spiritual than his acts reflect) ye shall be cast down into the pits of... well, losing a round. Not the worst fate, admittedly, but it isn't great, either.
+
+Each round the gods, fickle beings that they are, shall have a new value. And much like the "meaning of life" varies from person to person, you shall have a new target number as well.
+
+GO FORTH! And math well, my children. Math well.”
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,91 @@
+// Var to generate a random number, to populate the "numToGuess" span
+var targetNum = [];
+
+// variable to hold the running total of player accumulated points
+var score = 0;
+
+// var to hold number of wins
+var winCount = 0;
+
+// var to hold number of losses
+var lossCount = 0;
+
+// function to reset each round
+function reset() {
+    score = 0;
+    //empties the previous round's images from the containing div
+    $('#photos').empty();
+
+    //Generate the target number 
+    targetNum = Math.floor((Math.random() * 98) + 13);
+    $("#numToGuess").text(targetNum);
+    $("#currentNum").text(score);
+
+    // var to hold the possible values of images, between 1 and 12, creating an array of 4 integers
+    var numOpts = [Math.floor(Math.random() * (12 - 1)) + 1,
+        Math.floor(Math.random() * (12 - 1)) + 1,
+        Math.floor(Math.random() * (12 - 1)) + 1,
+        Math.floor(Math.random() * (12 - 1)) + 1
+    ];
+
+    // var to hold the possible images to which we assign values
+    var imageOptions = ["assets/images/budda.png",
+        "assets/images/virgin.png",
+        "assets/images/goddess.png",
+        "assets/images/ganesha.png"
+    ]
+
+    // for loop which assigns values to images, adds classes to the imgs, and puts the imgs into the empty div
+    for (var i = 0; i < imageOptions.length; i++) {
+
+        var imageGods = $("<img>");
+
+        $("#photos").append(imageGods);
+
+        imageGods.addClass("godsImage");
+
+        imageGods.addClass("img-rounded");
+
+        imageGods.attr("src", imageOptions[i]);
+
+        imageGods.attr("data-godsValue", numOpts[i]);
+
+    }
+
+    // this function is responsible for the increasing current number score, and also handles wins and losses
+    $(".img-rounded").on("click", function() {
+
+        var godsValue = ($(this).attr("data-godsValue"));
+
+        godsValue = parseInt(godsValue);
+
+        score += godsValue;
+
+        $("#currentNum").text(score);
+
+        if (score >= targetNum) {
+
+            if (score === targetNum) {
+                alert("You please me, child. My kingdom is yours.");
+                winCount++;
+                $("#wins").text(winCount)
+
+            } else if (score > targetNum) {
+                alert("Come, my child. You have much to learn.");
+                lossCount = lossCount + 1;
+                $("#losses").text(lossCount);
+            }
+
+            reset();
+
+        }
+
+
+    });
+}
+
+$(document).ready(function() {
+
+    reset();
+
+});
